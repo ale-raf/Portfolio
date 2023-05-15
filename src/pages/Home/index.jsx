798452a7a1257c7c30181f21a142d2fb27efbc73
Ale Raf <raffinalexis@gmail.com>
2023-05-15 16:34:03 +0200

    home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,9 @@
 import Header from '../../components/Header';
 import Banner from '../../components/Banner';
 import About from '../../components/About';
-<<<<<<< HEAD
 import Works from '../../components/Works';
-=======
-// import Works from '../../components/Works';
->>>>>>> 54fd0d8b0dbb8f866c82b50065f90a7e3062cba5
-// import Form from '../../components/Form';
+import Form from '../../components/Form';
+import Footer from '../../components/Footer';
 
 function Home() {
     return (
@@ -14,12 +11,9 @@ function Home() {
             <Header />
             <Banner />
             <About />
-<<<<<<< HEAD
             <Works />
-=======
-            {/* <Works /> */}
->>>>>>> 54fd0d8b0dbb8f866c82b50065f90a7e3062cba5
-        {/* <Form /> */}
+            <Form />
+            <Footer />
         </>
     )
 }
