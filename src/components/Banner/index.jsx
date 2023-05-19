bdd21039924c0ef53d7b5eb7f77e28f36d5ea809
Ale Raf <raffinalexis@gmail.com>
2023-05-19 11:54:47 +0200

    button style and animation

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,35 +1,28 @@
-<<<<<<< HEAD
 import { HashLink } from 'react-router-hash-link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowDown} from '@fortawesome/free-solid-svg-icons';
-=======
->>>>>>> 54fd0d8b0dbb8f866c82b50065f90a7e3062cba5
 import avatar from '../../assets/avatar.svg';
 import './style.scss';
 
 function Banner() {
     return (
-        <section className='banner_section'>
+        <section id='banner' className='banner_section'>
             <div className='banner_container'>
                 <img className='banner_avatar' src={avatar} alt='avatar Alexis'></img>
                 <div>
-<<<<<<< HEAD
-                    <h1>Développeur web front et back</h1>
+                    <h1>Développeur web frontend</h1>
                     <h2>Je réalise des projets d’intégration et de programmation pour des applications et des sites web.</h2>
                 </div>
             </div>
             <div className='banner_bottom'>
-                <HashLink smooth to='/#about'><button className='banner_scroll_btn'><FontAwesomeIcon icon={faArrowDown} /> Scrollez pour découvrir</button></HashLink>
-                <div className='banner_img'></div>
+                <HashLink smooth to='/#about'>
+                    <button className='banner_scroll_btn'>
+                        <FontAwesomeIcon icon={faArrowDown} className='animate__animated animate__flash animate__slow animate__infinite' /> Découvrir mon parcours
+                    </button>
+                </HashLink>
+                <div className='banner_img animate__animated animate__rubberBand'></div>
             </div>
             
-=======
-                    <h1>Développeur web front-end et back-end</h1>
-                    <h2>Je réalise des projets d’intégration et de programmation pour des applications et des sites web.</h2>
-                </div>
-            </div>
-            <div className='banner_img'></div>
->>>>>>> 54fd0d8b0dbb8f866c82b50065f90a7e3062cba5
         </section>
     )
 }
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -2,27 +2,29 @@ import './style.scss';
 
 function Form() {
     return (
-        <section className="form-container">
-            <form className='form' action="#">
-                <label>
-                    Nom : 
-                    <input type='text' name='name' />
-                </label>
-                <label>
-                    Email : 
-                    <input type='text' name='email' />
-                </label>
-                <label>
-                    Sujet : 
-                    <input type='text' name='subject' />
-                </label>
-                <label>
-                    Message : 
-                    <textarea type='text' name='message' />
-                </label>
-                <input type="submit" value="Envoyer" />
-            </form>
-            
+        <section id='contact' className='form_section'>
+            <div className="form_container">
+                <h2>contact</h2>
+                <form className='form' action="#">
+                    <label>
+                        Nom : 
+                        <input type='text' name='name' />
+                    </label>
+                    <label>
+                        Email : 
+                        <input type='email' name='email' />
+                    </label>
+                    <label>
+                        Sujet : 
+                        <input type='text' name='subject' />
+                    </label>
+                    <label>
+                        Message : 
+                        <textarea type='text' name='message' />
+                    </label>
+                    <input type="submit" value="Envoyer" />
+                </form>
+            </div>
         </section>
     )
 }
