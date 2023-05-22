bb3ec5f57b66d01d277a6b2fb3feda49e692c39d
Ale Raf <raffinalexis@gmail.com>
2023-05-22 11:48:49 +0200

    project page

--- src/pages/Project/index.jsx
+++ src/pages/Project/index.jsx
@@ -1,6 +1,8 @@
 import '../../datas/projects';
 import { useParams } from 'react-router-dom';
 import projects from '../../datas/projects';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 function Project() {
     const { slug } = useParams();
@@ -8,22 +10,26 @@ function Project() {
     const currentProject = projects.filter((project) => project.slug === slug);
 
     return (
-        <main>
-            {currentProject.map((item) => (
-                <div key={item.id} className='project_container'>
-                    <h2>{item.title}</h2>
-                    <h3>{item.subtitle}</h3>
-                    <ul>
-                        {item.technologies.map((tech) => (
-                            <li key={tech}>{tech}</li>
-                        ))}
-                    </ul>
-                    <p>{item.demands}</p>
-                    <p>{item.difficulties}</p>
-                    <p>{item.solutions}</p>
-                </div>
-            ))}
-        </main>
+        <>
+            <Header />
+            <main>
+                {currentProject.map((item) => (
+                    <div key={item.id} className='project_container'>
+                        <h2>{item.title}</h2>
+                        <h3>{item.subtitle}</h3>
+                        <ul>
+                            {item.technologies.map((tech) => (
+                                <li key={tech}>{tech}</li>
+                            ))}
+                        </ul>
+                        <p>{item.demands}</p>
+                        <p>{item.difficulties}</p>
+                        <p>{item.solutions}</p>
+                    </div>
+                ))}
+            </main>
+            <Footer />
+        </>
     )
 }
 
