d4574ec6d694c9673372990d095e4fe8d9322cec
Ale Raf <raffinalexis@gmail.com>
2023-06-05 12:00:00 +0200

    demos links

--- src/datas/projects.js
+++ src/datas/projects.js
@@ -1,6 +1,6 @@
 const projects = [
     {
-        id: 1,
+        id: 0,
         cover: require("../assets/Booki.png"),
         slug: "Booki-Creez-la-page-daccueil-dune-agence-de-voyage-avec-HTML-et-CSS",
         title: "Booki",
@@ -10,10 +10,11 @@ const projects = [
         demands: "intégration complète d’une page web statique conforme à la maquette donnée et dotée d'une interface responsive adaptée aux formats tablette et mobile",
         difficulties: ["suivi logique et cohérent de la maquette fournie", "positionnement des éléments", "format adapté à la taille de l'écran"],
         solutions: ["utilisation de Figma", "Flexbox", "media queries pour gérer l'aspect responsive"],
-        repository: 'https://github.com/ale-raf/OpenClassroomsProjet2.git'
+        repository: 'https://github.com/ale-raf/OpenClassroomsProjet2.git',
+        demo: 'https://ale-raf.github.io/OpenClassroomsProjet2/'
     },
     {
-        id: 2,
+        id: 1,
         cover: require("../assets/Sophie_Bluel.png"),
         slug: "Sophie-Bluel-Creez-une-page-web-dynamique-avec-Javascript",
         title: "Sophie Bluel",
@@ -23,10 +24,11 @@ const projects = [
         demands: "rendu navigateur dynamique avec récupération et affichage des données issues d’une API ; création d’une fenêtre modale et d’un formulaire de connexion côté client",
         difficulties: ["manipulation des éléments du DOM", "gestion des événements utilisateurs", "affichage des données récupérées depuis les requêtes HTTP côté client"],
         solutions: ["méthodes et écouteurs d'événements", "API fetch"],
-        repository: 'https://github.com/ale-raf/OpenClassroomsProjet3.git'
+        repository: 'https://github.com/ale-raf/OpenClassroomsProjet3.git',
+        demo: 'https://ale-raf.github.io/OpenClassroomsProjet3/'
     },
     {
-        id: 3,
+        id: 2,
         cover: require("../assets/Nina_Carducci.png"),
         slug: "Nina-Carducci-Debuggez-et-optimisez-un-site-de-photographe",
         title: "Nina Carducci",
@@ -36,10 +38,11 @@ const projects = [
         demands: "débug et optimisation des indices de performance, de référencement et d’accessibilité du site ; rédaction d’un cahier de recette",
         difficulties: ["recherche des bugs présents dans plusieurs fichiers", "traitement des différents facteurs d’optimisation"],
         solutions: ["utilisation de la console et du débogueur du navigateur", "gestion du poids des images et des librairies"],
-        repository: 'https://github.com/ale-raf/OpenClassroomsProjet5.git'
+        repository: 'https://github.com/ale-raf/OpenClassroomsProjet5.git',
+        demo: 'https://ale-raf.github.io/OpenClassroomsProjet5/'
     },
     {
-        id: 4,
+        id: 3,
         cover: require("../assets/Kasa.png"),
         slug: "Kasa-Creez-une-application-web-de-location-immobiliere-avec-React",
         title: "Kasa",
@@ -49,10 +52,11 @@ const projects = [
         demands: "développement complet d’une application côté client à l’aide de React ; initialisation du projet avec Create React App et configuration du routage de l’application",
         difficulties: ["affichage de plusieurs pages distinctes", "réutilisation d'éléments récurrents", "gestion des erreurs avec affichage d'une page d’erreur"],
         solutions: ["gestion des différentes routes", "utilisation des composants et des props", "affichage condiditionnel"],
-        repository: 'https://github.com/ale-raf/OpenClassroomsProjet6.git'
+        repository: 'https://github.com/ale-raf/OpenClassroomsProjet6.git',
+        demo: 'https://ale-raf.github.io/OpenClassroomsProjet6/'
     },
     {
-        id: 5,
+        id: 4,
         cover: require("../assets/Mon_Vieux_Grimoire.png"),
         slug: "Mon-Vieux-Grimoire-Developpez-le-backend-dun-site-de-notation-de-livres",
         title: "Mon Vieux Grimoire",
@@ -62,7 +66,8 @@ const projects = [
         demands: "le code côté client est déjà prêt ; la mission est de compléter l’application en réalisant le code côté serveur ; élaboration d’une API avec un modèle logique de données ; stockage sécurisé des données et mise en œuvre des opérations CRUD",
         difficulties: ["vérification du code sans le navigateur", "mise à jour des nouveaux livres ajoutés"],
         solutions: ["utilisation de Thunder Client pour tester les différentes fonctionnalités et opérations depuis le backend", "update pour mettre à jour les nouvelles données"],
-        repository: 'https://github.com/ale-raf/OpenClassroomsProjet7.git'
+        repository: 'https://github.com/ale-raf/OpenClassroomsProjet7.git',
+        demo: 'https://ale-raf.github.io/OpenClassroomsProjet7/'
     }
 ]
 
