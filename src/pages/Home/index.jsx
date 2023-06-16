f7562e89b5fa0ab526535acae69b3c3a5cc677fd
Ale Raf <raffinalexis@gmail.com>
2023-06-16 11:34:45 +0200

    Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,23 @@
+import HelmetComponent from '../../components/HelmetComponent';
 import Header from '../../components/Header';
 import Banner from '../../components/Banner';
 import About from '../../components/About';
 import Works from '../../components/Works';
-import Form from '../../components/Form';
+import ContactForm from '../../components/ContactForm';
 import Footer from '../../components/Footer';
 
 function Home() {
     return (
         <>
+            <HelmetComponent 
+                title='Alexis Raffin - Développeur web frontend' 
+                description='Accueil du site de Alexis Raffin, développeur web frontend'
+            />
             <Header />
             <Banner />
             <About />
             <Works />
-            <Form />
+            <ContactForm />
             <Footer />
         </>
     )
