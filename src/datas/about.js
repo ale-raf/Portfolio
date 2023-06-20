98136dc7629b78f7616637c2de963f442cb4d28b
Ale Raf <raffinalexis@gmail.com>
2023-06-20 11:39:54 +0200

    update datas

---
+++ src/datas/about.js
@@ -0,0 +1,17 @@
+export const aboutContent = [
+    {
+        text: "Bonjour, je m’appelle <b>Alexis</b> et je suis <b>développeur front-end junior</b>. Je réalise des <b>projets d’intégration responsive et de développement web</b>. Qu’il s’agisse de <b>site « vitrine » de type landing page ou de site dynamique et interactif</b>, je suis en mesure de vous accompagner dans vos différents projets."
+    },
+    { 
+        text: "Après avoir suivi une formation avancée en droit et science politique je me suis finalement réorienté vers un domaine qui m’attire depuis longtemps : <b>l’informatique</b>. Aussi j’ai recommencé par m’intéresser au <b>graphisme</b> avant d’envisager l’apprentissage du <b>développement web</b>."
+    },
+    {
+        text: "Dans un premier temps, comme beaucoup de reconvertis professionnels, j’ai appris les bases du développement de manière <b>autodidacte</b> en découvrant HTML et CSS. C’est seulement dans un second temps que j’ai poussé et concrétisé mon engagement à travers une <b>formation en ligne</b>. La réalisation de <b>projets pratiques</b> variés et inspirés par les demandes du <b>monde professionnel</b> m’a fourni une expérience non négligeable. Cependant, cela ne représente qu’un premier pas dans le développement web."
+    },
+    {
+        text: "Je suis désormais impatient de m’attaquer à des <b>missions authentiques</b> mais aussi de mener mes <b>propres projets</b> afin de <b>progresser</b> et de <b>continuer à apprendre</b> encore davantage. Je serai donc ravi de poursuivre l’aventure à vos côtés. Vous trouverez, ci-dessous, un aperçu des projets que j’ai pu réaliser au cours de ma formation."
+    },
+    {
+        text: "A bientôt, j’espère !"
+    },
+];
--- src/datas/skills.js
+++ src/datas/skills.js
@@ -1,6 +1,6 @@
 import { faCode, faServer, faScrewdriverWrench } from '@fortawesome/free-solid-svg-icons';
 
-const skills = [
+export const skillsList = [
     {
         icon: faCode,
         title: 'Front-end',
@@ -16,7 +16,7 @@ const skills = [
         title: 'Outils',
         items: ['Git & Github', 'Figma & Photoshop', 'SEO', 'Trello']
     }
-]
+];
 
-export default skills
+export const skillsIntro = "La formation que j'ai suivie m'a permis de découvrir et d'apprendre plusieurs langages de programmation et des outils utiles au développement web. Ainsi, j'ai pu acquérir des compétences variées et plutôt solides tournées notamment vers l'environnement JavaScript. Évidemment, ces connaissances constituent une base qui demande à être sans cesse actualisée, revue et complétée.";
 
