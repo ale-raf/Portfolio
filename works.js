d66e0a5526ce5771531bcd60ef7712833922e2a7
Ale Raf <raffinalexis@gmail.com>
2024-05-07 11:02:30 +0200

    liste des projets mise dans un fichier distinct

---
+++ works.js
@@ -0,0 +1,71 @@
+export const works = [
+  {
+    title: "Booki",
+    description: "Création de la page d'accueil d'une agence de voyage",
+    stack: [
+      "images/React.png",
+      "images/Sass.png",
+      "images/HTML5.png",
+      "images/CSS3.png",
+    ],
+    code: "https://github.com/ale-raf/OpenClassroomsProjet2",
+    demo: "https://ale-raf.github.io/OpenClassroomsProjet2/",
+  },
+  {
+    title: "Sophie Bluel",
+    description: "Création d'une page web dynamique",
+    stack: ["images/JavaScript.png", "images/HTML5.png", "images/CSS3.png"],
+    code: "https://github.com/ale-raf/OpenClassroomsProjet3",
+    demo: null,
+  },
+  {
+    title: "Nina Carducci",
+    description: "Débogage et optimisation d'un site de photographe",
+    stack: ["images/JavaScript.png", "images/HTML5.png", "images/CSS3.png"],
+    code: "https://github.com/ale-raf/OpenClassroomsProjet5",
+    demo: "https://ale-raf.github.io/OpenClassroomsProjet5/",
+  },
+  {
+    title: "Kasa",
+    description: "Création d'une application web de location immobilière",
+    stack: ["images/React.png", "images/CSS3.png"],
+    code: "https://github.com/ale-raf/OpenClassroomsProjet6",
+    demo: "https://ale-raf.github.io/OpenClassroomsProjet6/",
+  },
+  {
+    title: "Mon vieux grimoire",
+    description: "Développement du back-end d'un site de notation de livres",
+    stack: ["images/Express.png", "images/MongoDB.png"],
+    code: "https://github.com/ale-raf/OpenClassroomsProjet7",
+    demo: null,
+  },
+  {
+    title: "GameOn",
+    description: "Création d'une landing page avec JavaScript",
+    stack: ["images/JavaScript.png", "images/HTML5.png", "images/CSS3.png"],
+    code: "https://github.com/ale-raf/OpenClassrooms_GameOn",
+    demo: "https://ale-raf.github.io/OpenClassrooms_GameOn",
+  },
+  {
+    title: "Fisheye",
+    description:
+      "Création d'un site accessible pour une plateforme de photographe",
+    stack: ["images/JavaScript.png", "images/HTML5.png", "images/CSS3.png"],
+    code: "https://github.com/ale-raf/OpenClassrooms_Fisheye",
+    demo: "https://ale-raf.github.io/OpenClassrooms_GameOn",
+  },
+  {
+    title: "Where in the world",
+    description: "Défi consistant à manipuler des données issues d'une API",
+    stack: ["images/JavaScript.png", "images/HTML5.png", "images/CSS3.png"],
+    code: "https://github.com/ale-raf/REST-countries-API",
+    demo: "https://ale-raf.github.io/REST-countries-API",
+  },
+  {
+    title: "Yummy Nouilles",
+    description: "Création et intégration de la maquette ",
+    stack: ["images/PHP.png", "images/HTML5.png", "images/CSS3.png"],
+    code: "https://github.com/ale-raf/yummy-nouilles",
+    demo: null,
+  },
+];
